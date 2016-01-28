778ccd140661f17fef298c552e463130213d633d
マツシタ コウタロウ <kouta.dvlp@icloud.com>
2016-01-28 20:32:35 +0900

    first commit

--- ぽけメモ2/FirstViewController.swift
+++ ぽけメモ2/FirstViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController , UITableViewDataSource, UITableViewDelegate,InputTextTableCellDelegate{
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,21 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 10;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cell: InputTextTableCell = tableView.dequeueReusableCellWithIdentifier("InputTextCell", forIndexPath: indexPath)
+        let cell: InputTextTableCell = tableView.dequeueReusableCellWithIdentifier("InputTextCell", forIndexPath: indexPath) as! InputTextTableCell
+        cell.delegate = self
+        return cell
+    }
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return InputTextTableCell.height()
 }
-
+    func textFieldDidEndEditing(cell: InputTextTableCell, value: NSString) -> () {
+        let path = tableView.indexPathForRowAtPoint(cell.convertPoint(cell.bounds.origin, toView: tableView))
+        NSLog("row = %d, value = %@", path!.row, value)
+    }
+}
